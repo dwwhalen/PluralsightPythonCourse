cecbdc19552cf0a1292d01d55d955503ffdbdd2a
Dennis Whalen <dwwhalen@gmail.com>
2017-09-29 13:24:43 -0400

    Finished the course

--- student.py
+++ student.py
@@ -1,18 +1,22 @@
 class Student:
-    students = []
+    #students = []
 
     school_name = "Springfield Elementary"
 
-    def __init__(self, name, student_id=332):
-        self.name = name
+    def __init__(self, first_name, last_name, student_id=332):
+        self.first_name = first_name
+        self.last_name = last_name
         self.student_id = student_id
-        self.students.append(self)
+        #self.students.append(self)
 
     def __str__(self):
-        return "Student " + self.name
+        return "Student " + self.last_name
 
-    def get_name_capitalized(self):
-        return self.name.capitalize()
+    def get_firstname_capitalized(self):
+        return self.first_name.capitalize()
+
+    def get_lastname_capitalized(self):
+        return self.first_name.capitalize()
 
     def get_school_name(self):
         return self.school_name
